f66a106404075876d90d8ae2ca3ea6ecc52ae1e1
Alexander <lord-squirrel@protonmail.com>
2023-10-16 21:45:02 +0200

    take command line input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use std::env;
 mod core;
 
 fn main() {
-    println!("{}", core::encode::to_string("1234.25").unwrap());
+    let args: Vec<String> = env::args().collect();
+
+    if let Some(input) = args.get(1) {
+        match core::encode::to_string(input) {
+            Ok(res) => println!("Guldhjerte code: {}", res),
+            Err(e) => panic!("Error: {}", e),
+        }
+    } else {
+        panic!("No input provided");
+    }
 }
